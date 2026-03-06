69a2beb5bcb7d3e41aad71863fbd4e17bc0453b2
lovvgh <btw18@sunyorange.edu>
2026-03-06 14:46:23 -0500

    Add labs.lab3
    Add additional assignment file.

---
+++ src/assignments/arraybag/ArraySet.java
@@ -0,0 +1,43 @@
+package assignments.arraybag;
+
+public class ArraySet<T> implements SetInterface<T> {
+    @Override
+    public int getCurrentSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean add(T newEntry) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(T anEntry) {
+        return false;
+    }
+
+    @Override
+    public T remove() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(T anEntry) {
+        return false;
+    }
+
+    @Override
+    public T[] toArray() {
+        return null;
+    }
+}
---
+++ src/labs/lab3/src/DummyEvent.java
@@ -0,0 +1,33 @@
+package labs.lab3.src;
+
+/**
+ * This class will be used to perform tests on the SimulationEventQueue class.
+ *
+ * @author Charles Hoot
+ * @version 5.0
+ */
+
+import labs.lab3.src.QueuePackage.*;
+
+    // Inherit from the abstract SimulationEvent.  Only the constructor and process will
+    // be defined.
+
+    public class DummyEvent extends SimulationEvent
+    {
+        public DummyEvent (double theTime, String action)
+        {
+            super(theTime,action);
+        }
+         	
+    	/**
+    	 * Process the event.
+    	 */
+    	public void process()
+    	{
+    	   System.out.print("Proccessing event with time " +
+                   this.getTime() + 
+                   " with action " + 
+                   this.getDescription());
+	}
+
+    }  // end of DummyEvent
